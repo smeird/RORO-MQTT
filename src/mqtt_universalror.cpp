dfcf8866692d9a65c66b6d563361bc518a9059b7
smeird <dom@smeird.com>
2025-08-29 12:29:06 +0100

    Add configurable MQTT topics

--- src/mqtt_universalror.cpp
+++ src/mqtt_universalror.cpp
@@ -1,5 +1,6 @@
 #include "mqtt_universalror.h"
 #include <chrono>
+#include <cstring>
 #include <thread>
 
 using namespace std::chrono_literals;
@@ -19,11 +20,19 @@ bool MQTTUniversalROR::initProperties() {
     INDI::Dome::initProperties();
     addDebugControl();
     // Topics defaults
-    topic_open_ = "observatory/roof/cmd/open";
-    topic_close_ = "observatory/roof/cmd/close";
-    topic_stop_ = "observatory/roof/cmd/stop";
-    topic_limit_open_ = "observatory/roof/limit/open";
-    topic_limit_closed_ = "observatory/roof/limit/closed";
+    IUFillText(&mqtt_topics_t_[0], "OPEN", "Open", "observatory/roof/cmd/open");
+    IUFillText(&mqtt_topics_t_[1], "CLOSE", "Close", "observatory/roof/cmd/close");
+    IUFillText(&mqtt_topics_t_[2], "STOP", "Stop", "observatory/roof/cmd/stop");
+    IUFillText(&mqtt_topics_t_[3], "LIMIT_OPEN", "Limit Open", "observatory/roof/limit/open");
+    IUFillText(&mqtt_topics_t_[4], "LIMIT_CLOSED", "Limit Closed", "observatory/roof/limit/closed");
+    IUFillTextVector(&mqtt_topics_tp_, mqtt_topics_t_, 5, getDeviceName(), "MQTT_TOPICS", "MQTT Topics", "Configuration", IP_RW, 60, IPS_IDLE);
+    loadConfig(true);
+    topic_open_ = mqtt_topics_t_[0].text;
+    topic_close_ = mqtt_topics_t_[1].text;
+    topic_stop_ = mqtt_topics_t_[2].text;
+    topic_limit_open_ = mqtt_topics_t_[3].text;
+    topic_limit_closed_ = mqtt_topics_t_[4].text;
+    defineText(&mqtt_topics_tp_);
     return true;
 }
 
@@ -59,6 +68,29 @@ IPState MQTTUniversalROR::UnPark() {
 bool MQTTUniversalROR::Abort() {
     fsm_.command_stop();
     publish_command(topic_stop_);
+    return true;
+}
+
+bool MQTTUniversalROR::ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n) {
+    if (strcmp(dev, getDeviceName()) == 0) {
+        if (!strcmp(name, mqtt_topics_tp_.name)) {
+            IUUpdateText(&mqtt_topics_tp_, texts, names, n);
+            topic_open_ = mqtt_topics_t_[0].text;
+            topic_close_ = mqtt_topics_t_[1].text;
+            topic_stop_ = mqtt_topics_t_[2].text;
+            topic_limit_open_ = mqtt_topics_t_[3].text;
+            topic_limit_closed_ = mqtt_topics_t_[4].text;
+            mqtt_topics_tp_.s = IPS_OK;
+            IDSetText(&mqtt_topics_tp_, nullptr);
+            return true;
+        }
+    }
+    return INDI::Dome::ISNewText(dev, name, texts, names, n);
+}
+
+bool MQTTUniversalROR::saveConfigItems(FILE *fp) {
+    INDI::Dome::saveConfigItems(fp);
+    IUSaveConfigText(fp, &mqtt_topics_tp_);
     return true;
 }
 
